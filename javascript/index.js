7f7e1351b67caa159164d9d7790dfc4f72e779a0
Rishavvsingh <rishavsingh.10988@gmail.com>
2025-01-21 22:03:47 +0530

    index.js

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+console.log('Hello JavaScript');
+
+num = 56.34;
+
+console.log(num);
+
+console.log(typeof num);
+
+myname = "Rishav";
+
+console.log(myname);
+
+// new arithmetic operators
+console.log(3 ** 4);
+
+console.log(1 + 1);
+console.log('1' + '1');
+console.log('1' + 1);
+console.log('1' + false);
+console.log(1 + 1 + '1');
+console.log(1 == 1);
+console.log('1' == '1');
+console.log('1' == 1);
+console.log('1' === 1);
+
+
