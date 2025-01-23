314aa3d767f1a01b3d0644e92bb94c1c57eb8d69
Rishavvsingh <rishavsingh.10988@gmail.com>
2025-01-23 14:05:15 +0530

    loops

--- javascript/loops.js
+++ javascript/loops.js
@@ -20,3 +20,24 @@ do {
     console.log(b);
     b++;
 } while (b < 20);
+
+// WAP to reverse a number
+console.log('reverse a number');
+
+
+let num = 37354;
+let reverse = 0;
+
+while(num>0){
+    console.log(reverse);
+    console.log(num);
+    
+    let d = num % 10;
+    reverse = reverse*10+d;
+    num = parseInt(num/10);
+}
+ 
+console.log(reverse);
+
+
+
