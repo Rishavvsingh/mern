e2cbb1a968b1bf2a92a2eefe05666923687ea039
Rishavvsingh <rishavsingh.10988@gmail.com>
2025-01-28 21:14:22 +0530

    filter

--- javascript/map_filter.js
+++ javascript/map_filter.js
@@ -30,3 +30,20 @@ console.log( parseInt('₹62546.765'.slice(1)) );
 const intPrices = rawPrices.map( (p) => { return parseInt(p.slice(1)) } );
 
 console.log(intPrices);
+
+// filter operation
+// [2, 4, 8, 7, 4, 23, 21]
+const oddNums = myarr.filter((n) => { return n % 2 !== 0 })
+console.log(oddNums);
+
+// [ 23000, 4500, 2800, 1850, 1724 ]
+// WAP to filter out all prices between 10000 and 2000
+const budgetPrices = prices.filter((p) => {
+    return p < 10000 && p > 2000
+});
+console.log(budgetPrices);
+
+// [ 'Ramu', 'Raju', 'Kaliya', 'Pinki' ]
+// WAP to filter all names having length equal to or greater than 5
+const bigNames = names.map((n) => { return n.length >= 5 });
+console.log(bigNames);
