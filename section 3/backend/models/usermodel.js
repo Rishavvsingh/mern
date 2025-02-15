aa353e5b0012fd12db63db3678d3df3aa90b930d
Rishavvsingh <rishavsingh.10988@gmail.com>
2025-02-15 21:32:01 +0530

    read operation

--- section 3/backend/models/usermodel.js
+++ section 3/backend/models/usermodel.js
@@ -4,6 +4,7 @@ const mySchema = new Schema({
     name: String,
     email: { type: String, unique: true },
     password: { type: String, require: true },
+    city: { type: String, default: 'unknown'}, 
     createdAt: { type: Date, default: Date.now }
 });
 
