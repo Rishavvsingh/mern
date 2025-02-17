896eea949fbc4dbac1582940a7337635ec1c7c26
Rishavvsingh <rishavsingh.10988@gmail.com>
2025-02-17 22:14:02 +0530

    product router data add

---
+++ section 3/backend/routers/productRouter.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const Model = require('../models/productModel');
+const { model } = require('mongoose');
+
+const router = express.Router();
+
+router.post('/add', (req, res) => {
+    console.log(req.body);
+
+    new Model(req.body).save()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
+});
+
+//getall
+router.get('/getall', (req, res) => {
+
+    Model.find()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+            
+         })
+});
+
+//getbyid
+router.get('/getbyid', (req, res) => {
+   Model.findById(req.params.id)
+       .then((result) => {
+           res.status(200).json(result);
+          }).catch((err) => {
+           console.log(err);
+           res.status(500).json(err);
+          }); 
+});
+
+//update
+router.get('/update', (req, res) => {
+    res.send('response from product update')
+});
+
+//delete
+router.get('/delete', (req, res) => {
+    res.send('response from product delete')
+});
+
+module.exports = router;
